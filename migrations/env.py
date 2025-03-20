4e45dae4b35d69be9969ae76ed0c6ec145e33d68
Plummy <plummybeatsoff@gmail.com>
2025-03-20 16:36:16 +0100

    Configure alembic.ini and env

--- migrations/env.py
+++ migrations/env.py
@@ -5,9 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.database.models import Base
+from config import Config
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+app_config = Config(_env_file=".env")
+config.set_section_option(config.config_ini_section, "DATABASE_DSN", app_config.database_dsn.get_secret_value())
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
